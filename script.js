b9c66b316220a743ff7d41ab0048d521f9bfd839
estefanio-git <MIKOENGONGABINDANG@gmail.com>
2026-08-27 15:53:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("JavaScript carregado com sucesso!");
+    const copyBtn = document.getElementById("copy-btn");
+    const emailText = document.getElementById("email-text").innerText;
+
+    // Função que copia o e-mail para a área de transferência ao clicar no botão
+    copyBtn.addEventListener("click", () => {
+        navigator.clipboard.writeText(emailText).then(() => {
+            const textOriginal = copyBtn.innerText;
+            
+            copyBtn.innerText = "Copiado! ";
+            copyBtn.style.backgroundColor = "#28a745";
+
+            setTimeout(() => {
+                copyBtn.innerText = textOriginal;
+                copyBtn.style.backgroundColor = "#007bff";
+            }, 2000);
+        });
+    });
 });
